efb92bfff8854d6227bd99264ea143cf819410c7
Colin Campbell <prontobol@gmail.com>
2021-04-08 20:06:33 -0500

    finally added items in correct order

--- src/com/colin/Main.java
+++ src/com/colin/Main.java
@@ -1,12 +1,31 @@
 package com.colin;
 
-import com.colin.Screens.HomeScreen;
 import com.colin.Screens.ListPromotersScreen;
+import com.colin.Screens.ListPromotersScreen1;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class Main {
 
     public static void main(String[] args) {
-        ListPromotersScreen promotersScreen = new ListPromotersScreen();
+        ListPromotersScreen1 screen = new ListPromotersScreen1();
+
+        /**
+         * DefaultTableModel tableModel = new DefaultTableModel();
+         *         JTable table = new JTable(tableModel);
+         *         tableModel.addColumn("Languages");
+         *         tableModel.insertRow(0, new Object[] { "CSS" });
+         *         tableModel.insertRow(0, new Object[] { "HTML5" });
+         *         tableModel.insertRow(0, new Object[] { "JavaScript" });
+         *         tableModel.insertRow(0, new Object[] { "jQuery" });
+         *         tableModel.insertRow(0, new Object[] { "AngularJS" });
+         *         tableModel.insertRow(tableModel.getRowCount(), new Object[] { "ExpressJS" });
+         *         JFrame f = new JFrame();
+         *         f.setSize(550, 350);
+         *         f.add(new JScrollPane(table));
+         *         f.setVisible(true);
+         */
 
     }
 }
--- src/com/colin/Screens/ListPromotersScreen.java
+++ src/com/colin/Screens/ListPromotersScreen.java
@@ -1,84 +1,31 @@
 package com.colin.Screens;
 
 import javax.swing.*;
-import javax.swing.event.TableModelListener;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
-import java.awt.*;
 
-public class ListPromotersScreen extends JFrame {
+public class ListPromotersScreen extends JDialog {
+    private JPanel contentPane;
+    private JTable table1;
+    private JButton backButton;
+    private JTextField searchByIDTextField;
+    private JButton searchIDButton;
+    private JButton sortByIDButton;
+    private JButton sortByNameButton;
+    private JButton buttonOK;
+    private JButton buttonCancel;
 
-    private JButton backButton = new JButton("<---");
-    private JButton sortByIDButton = new JButton("Sort By ID");
-    private JButton sortByBudgetButton = new JButton("Sort By Budget");
-    private JTextField searchByIDField = new JFormattedTextField();
-    private JTable listPromotersTable = new JTable();
+    public ListPromotersScreen() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
 
-    public ListPromotersScreen()
-    {
-        searchByIDField.setColumns(20);
-
-        setTitle("Promoter");
-        setSize(new Dimension(900,600));
-
-        // This layout makes everything looks how it should
-        BorderLayout borderLayout = new BorderLayout();
-        setLayout(borderLayout);
-
-        // Methods are used to short hand setting up and creating components to reduce repeating code
-        JPanel leftFloatPanel = createFloatPanel(FlowLayout.LEFT,backButton);
-        JPanel rightFloatPanel = createFloatPanel(FlowLayout.RIGHT,searchByIDField,sortByBudgetButton,sortByBudgetButton);
-        JPanel topBarComponents = createTopBarPanel(leftFloatPanel,rightFloatPanel);
-
-        // Add to the top of the screen
-        add(topBarComponents,BorderLayout.PAGE_START);
-
-        // Now set up the table
-
-        Object[] row = { "1", "Colin", "5000" };
-        Object[][] data = {row};
-
-        DefaultTableModel tableModel = new DefaultTableModel(data,new String[]{"Promoter ID","Promoter Name","Promoter Budget"});
-
-        listPromotersTable.setModel(tableModel);
-        listPromotersTable.setPreferredSize(new Dimension(1000,500));
-
-        JScrollPane jScrollPane = new JScrollPane();
-
-        jScrollPane.add(listPromotersTable);
-        jScrollPane.setPreferredSize(new Dimension(1000,500));
-        // Add to the bottom of the screen
-        add(jScrollPane,BorderLayout.PAGE_END);
-
+        setSize(1080,720);
         setVisible(true);
-        // Get's messy if the user can resize this screen, so no they won't resize anything
-        setResizable(false);
     }
 
-
-    /**
-     * @param containers List of Containers to add to the top panel of the screen
-     * @return A JPanel that contains the widgets for the top screen
-     */
-    private JPanel createTopBarPanel(Container ...containers)
-    {
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        for (Container container : containers)
-            panel.add(container);
-        return panel;
-    }
-
-    private JPanel createFloatPanel(int floatDirection,Container ...containers)
-    {
-        JPanel panel = new JPanel();
-        FlowLayout layout = new FlowLayout();
-        layout.setAlignment(floatDirection);
-        panel.setLayout(layout);
-
-        for (Container container : containers)
-            panel.add(container);
-
-        return panel;
+    public static void main(String[] args) {
+        ListPromotersScreen dialog = new ListPromotersScreen();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
     }
 }
